6ee0f84b6dd9e0739d0e627d172c9ccca3e8b134
Slurpeesh <JeffNFG@mail.ru>
2023-01-03 21:09:21 +0500

    site prototype

---
+++ scripts/lang.js
@@ -0,0 +1,36 @@
+const langArr = {
+    "title": {
+        "en": "Decision making under uncertainty",
+        "ru": "Принятие решений в условии неопределенности"
+    },
+
+    "sizeInput": {
+        "en": "Please, input the size of a matrix:",
+        "ru": "Введите размерность матрицы:"
+    },
+
+    "accept": {
+        "en": "Accept",
+        "ru": "Принять"
+    },
+
+    "findSolutions": {
+        "en": "Find solutions",
+        "ru": "Найти решения"
+    },
+
+    "matSizeError": {
+        "en": "Incorrectly entered values of the matrix dimension",
+        "ru": "Неправильно введены значения размерности матрицы"
+    },
+
+    "matValueError": {
+        "en": "Incorrectly entered matrix values",
+        "ru": "Неправильно введены значения матрицы"
+    },
+
+    "hugeMatSize": {
+        "en": "The size of a matrix is too big",
+        "ru": "Слишком большая размерность матрицы"
+    }
+}
